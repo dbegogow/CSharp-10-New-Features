07600731d69e30138e6e2aa1d3039ba411116505
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-04-07 17:07:47 +0300

    Add demo for record types seal ToString

---
+++ RecordTypesSealToString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RecordTypesSealToString;
+
+internal record Rectangle(int Height, int Width)
+{
+    public sealed override string ToString()
+    {
+        return $"Height is: {this.Height} and width is: {this.Width}";
+    }
+}
+
+internal record Square : Rectangle
+{
+    protected Square(int sideLength) : base(sideLength, sideLength)
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"Side length: {Height}";
+    }
+}
