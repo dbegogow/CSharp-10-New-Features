b1be45b46a6156935615d213ab3afe626ff2e8c9
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-04-07 16:46:10 +0300

    Add extended property patterns demo

---
+++ ExtendedPropertyPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExtendedPropertyPatterns;
+
+var rectangleInside = new Rectangle(100, 100);
+
+var rectangle = new Rectangle(200, 300, rectangleInside);
+
+if (rectangle is { rectangle.Height: > 100 })
+{
+
+}
---
+++ ExtendedPropertyPatterns/Rectangle.cs
@@ -0,0 +1,3 @@
+﻿namespace ExtendedPropertyPatterns;
+
+internal record Rectangle(int Height, int Width, Rectangle? rectangle = null);
