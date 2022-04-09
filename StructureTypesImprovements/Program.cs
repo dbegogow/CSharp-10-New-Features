2bea7da3f12359891f2e4cc0ea63e562baf06924
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-04-09 14:05:06 +0300

    Add demo for structure type improvements

---
+++ StructureTypesImprovements/Program.cs
@@ -0,0 +1,5 @@
+﻿using StructureTypesImprovements;
+
+var rectangle = new Rectangle(400, 70);
+
+var newOne = rectangle with { Width = 400 };
---
+++ StructureTypesImprovements/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace StructureTypesImprovements;
+
+public struct Rectangle
+{
+    public Rectangle()
+    {
+        this.Height = 0;
+        this.Width = 0;
+    }
+
+    public Rectangle(int height, int width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public int Height { get; init; }
+
+    public int Width { get; init; }
+}
